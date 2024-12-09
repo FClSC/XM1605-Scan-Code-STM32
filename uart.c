431a741f86b189acab56db1c6744c893434f8ac5
FClSC <153213152+FClSC@users.noreply.github.com>
2024-12-09 22:09:45 +0800

    12/9

---
+++ uart.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stdarg.h"    
+#include "string.h" 
+#include "uart.h"
+
+
+uint8_t Serial_Flag5=0;//标志位
+char UART5_RX_BUF[100];//扫码获得存储区
+
+
+//获得UART5_RX_BUF的0-6位是所需要的  ：  123+321
+//串口五    扫码模块   采用USART5 
+//单片机   PC12 TX    PD2 RX   
+
+void UART5_Init(void)
+{
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART5, ENABLE);//启用UART5的APB1时钟
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+	//PC12   TX 
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	//PD2   RX
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_Init(UART5, &USART_InitStructure);
+	
+	USART_ITConfig(UART5, USART_IT_RXNE, ENABLE);
+	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = UART5_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_Init(&NVIC_InitStructure);
+
+
+	
+	USART_Cmd(UART5, ENABLE);
+}
+
+/********************************
+函数功能 : 串口5 发送一个字节
+输入参数 : 一个字节
+输出参数 ；无
+*********************************/
+void USART5_SendBits(uint8_t data)
+{
+	UART5->DR = data;
+	while(USART_GetFlagStatus(UART5,USART_FLAG_TXE) == RESET);
+}
+/********************************
+函数功能 : 串口5 发送一个数组
+输入参数 : 一个数组
+输出参数 ；无
+*********************************/
+void UART5_SendArray(uint8_t *array,uint8_t length)
+{
+	uint8_t i;
+	for(i=0;i<length;i++)
+	{
+		USART5_SendBits(array[i]);
+	}
+}
+/********************************
+函数功能 : 串口5 发送一个字符串
+输入参数 : 一个字符串
+输出参数 ；无
+*********************************/
+void UART5_SendString(uint8_t *String1)
+{
+	uint8_t i;
+	for(i=0;String1[i]!='\0';i++)
+	{
+		USART5_SendBits(String1[i]);		
+	}
+}
+
+
+//串口5获得标志位
+uint8_t Serial5_GetRxFlag(void)
+{
+	if(Serial_Flag5 == 1)
+	{
+		Serial_Flag5=0;
+		return 1;
+	}
+	return 0;
+}
+// @-------#$
+void UART5_IRQHandler(void)
+{
+	static uint8_t RxState = 0;
+	static uint8_t pRxPacket = 0;	
+    if (USART_GetITStatus(UART5, USART_IT_RXNE) == SET)
+	{
+        uint8_t RxData = USART_ReceiveData(UART5);
+        if(RxState==0)
+		{
+			if(RxData=='@'&&Serial_Flag5==0)
+			{
+				RxState=1;
+				pRxPacket=0;		
+			}			
+		}
+		else if (RxState==1)
+		{
+			if(RxData=='#')
+			{
+				RxState=2;
+			}
+			else
+			{
+				UART5_RX_BUF[pRxPacket]=RxData;
+				pRxPacket++;
+			}
+		}
+		else if(RxState==2)
+		{
+			if(RxData=='$')
+			{
+				RxState=0;
+				UART5_RX_BUF[pRxPacket]='\0'; //存储完毕后为字符串形式在缓存区
+				Serial_Flag5=1;				
+			}
+			
+			
+		}
+        USART_ClearITPendingBit(UART5, USART_IT_RXNE);
+    }
+}
+
+
+void UART5_Start_Scan(void)//发送扫码指令
+{
+
+   uint8_t Order[9]={0x7E,0x00,0x08,0x01,0x00,0x02,0x01,0xAB,0xCD};
+   UART5_SendArray(Order,9);
+
+}
+
+
+void UART5_PraseCode(const char *Buf, uint16_t *code1, uint16_t *code2) //解析“123+321”为整形 123和321保存于code1和code2中
+{
+
+    char tempBuf[256];
+    strcpy(tempBuf, Buf);
+
+    char *plusSign = strchr(tempBuf, '+');
+    if (plusSign != NULL) 
+    {
+        *plusSign = '\0';
+        *code1 = atoi(tempBuf);
+        *code2 = atoi(plusSign + 1);
+    } 
+}
---
+++ uart.h
@@ -0,0 +1,19 @@
+#ifndef _UART_H
+#define _UART_H
+
+extern char UART5_RX_BUF[100];
+extern uint8_t Serial_Flag5;
+
+
+void UART5_Init(void);
+void USART5_SendBits(uint8_t data);
+void UART5_SendArray(uint8_t *array,uint8_t length);
+void UART5_SendString(uint8_t *String1);
+uint8_t Serial5_GetRxFlag(void);
+void  UART5_IRQHandler(void);
+void UART5_Start_Scan(void);
+void UART5_PraseCode(const char *Buf, uint16_t *code1, uint16_t *code2) ;
+
+
+
+#endif
